9fa0871c38556880f7cdecdd3ceebdbf329d29b3
Kurt Stephens <ks.github@kurtstephens.com>
2013-10-19 18:38:40 -0500

    Added constant-expr?, random-subexpression.

--- src/closol/expr.clj
+++ src/closol/expr.clj
@@ -15,3 +15,17 @@
     (symbol? expr) 1
     :else          0))
 
+(defn constant-expr? [expr]
+  "True if expr or its subexpressions are constant."
+  (cond
+    (number? expr) true
+    (list? expr)   (every? constant-expr? (rest expr))
+    :else          false))
+
+(defn random-subexpression [expr root]
+  "Selects a random subexpression from root expression of the same complexity as expr.
+Returns expr if one of similar complexity cannot be found."
+  (let [expr-c     (expression-complexity expr)
+        valid-exps (filter (fn [e] (= (expression-complexity e) expr-c))
+                     (enumerate-subexpressions root))]
+    (if (empty? valid-exps) expr (rand-nth valid-exps))))
