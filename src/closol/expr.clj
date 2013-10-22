3ecbf5414731f16c67835bc46a2c21273284287a
Kurt Stephens <ks.github@kurtstephens.com>
2013-10-21 20:26:43 -0500

    seq?.

--- src/closol/expr.clj
+++ src/closol/expr.clj
@@ -4,41 +4,34 @@
   "Enumerates all improper subexpressions, including expr."
   [expr]
   (cond
-    (list? expr)
-      (if (empty? expr) '()
-        (cons expr (apply concat (map subexpressions (rest expr)))))
-    :else (list expr)))
+    (seq? expr)  (if (empty? expr) '()
+                   (cons expr
+                     (apply concat
+                       (map subexpressions (rest expr)))))
+    :else        (list expr)))
 
 (defn expression-complexity
   "A basic complexity metric."
   [expr]
   (cond
-    (list? expr)   (reduce + 2 (map expression-complexity (rest expr)))
-    (symbol? expr) 1
-    :else          0))
+    (seq? expr)     (reduce + 2 (map expression-complexity (rest expr)))
+    (symbol? expr)  1
+    :else           0))
 
 (defn constant-expr?
   "Is expression constant?"
   [expr]
   (cond
     (number? expr) true
-    (list? expr)   (every? constant-expr? (rest expr))
+    (seq? expr)    (every? constant-expr? (rest expr))
     :else          false))
 
 (defn constant-fold
   "Folds constant expressions within."
   [expr]
   (cond
-    (list? expr)
-    (if (constant-expr? expr) (eval expr)
-      (cons (first expr) (map constant-fold (rest expr))))
-    :else expr))
+    (seq? expr)  (if (constant-expr? expr)
+                   (eval expr)
+                   (map constant-fold expr))
+    :else        expr))
 
-(defn random-subexpression-of-complexity
-  "Selects a random subexpression from root expression of the same complexity as expr.
-Returns expr if one of similar complexity cannot be found."
-  [expr root]
-  (let [ expr-c     (expression-complexity expr)
-         valid-exps (filter #(= (expression-complexity %1) expr-c)
-                     (subexpressions root))]
-    (if (empty? valid-exps) expr (rand-nth valid-exps))))
