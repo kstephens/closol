b0fdb6b291f4a8c7951ad60c68b56e7c75637bc1
Kurt Stephens <kstephens@drw.com>
2015-11-17 01:03:36 -0500

    Subexpression memoization at parameter combinations.

---
+++ src/closol/memo_expr.clj
@@ -0,0 +1,82 @@
+(ns closol.memo-expr
+  (:require
+   [clojure.set :as set]
+   [clojure.walk :refer :all]))
+
+(def ^:const parameter?    symbol?)
+(def ^:const application?  seq?)
+(def ^:const function      first)
+(def ^:const arguments     rest)
+(def ^:const constant?     number?)
+
+(defn append!
+  [trans col]
+  (doseq [e col]
+    (conj! trans e))
+  trans)
+
+(declare parameters)
+
+(defn parameters-of
+  [es]
+  (apply set/union (map parameters es)))
+
+(defn parameters
+  [e]
+  (cond
+    (parameter?   e)  #{e}
+    (application? e)  (parameters-of (arguments e))
+    :else             #{}))
+
+(defn parameterized?
+  [e]
+  (and (application? e) (not (empty? (parameters e)))))
+
+(defn subexpressions
+  [e]
+  (if (application? e) (arguments e) []))
+
+(defn memoize-subexpressions?
+  [e]
+  (and (not (parameter? e))
+       (> (count (parameters e))
+          (apply max 0 (map #(count (parameters %)) (subexpressions e))))))
+
+(def generate-variable gensym)
+
+(defn memoized-subexpression
+  [se]
+  (if (parameterized? se)
+    (let [f        (generate-variable)
+          params   (sort (parameters se))
+          func     `(memoize (fn ~(vec params) ~se))
+          expr     (cons f params)]
+      (list expr f func))
+    (list se false false)))
+
+
+(declare memoize-traverse)
+
+(defn memoize-application
+  [e fns]
+  (let [e (cons (function e) (map #(memoize-traverse % fns) (arguments e)))]
+    (if (memoize-subexpressions? e)
+      (let [memos  (map memoized-subexpression (subexpressions e))]
+        (append! fns (filter second memos))
+        (cons (function e) (map first memos)))
+      e)))
+
+(defn memoize-traverse
+  [e fns]
+  (if (application? e)
+    (memoize-application e fns)
+    e))
+
+(defn memoize-expression
+  [params tle]
+  (let [fns    (transient [])
+        tle2   (memoize-traverse tle fns)
+        fns    (persistent! fns)]
+    `(let ~(vec (mapcat rest fns))
+       (fn ~(vec params) ~tle2))))
+
